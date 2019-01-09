d8004ea15ef156dcaddf28061f7bc8f5f4b4ed84
Nsubuga Solomon <snsubuga@live.com>
2019-01-10 01:54:41 +0300

    chore(models): Create models
    - add category and article models
    - register category and article models on the admin site
    - write tests

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Article
 
-# Register your models here.
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name', 'created', )
+
+
+@admin.register(Article)
+class AdminArticle(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'category',
+                    'image', 'writer', 'status', 'created', )
+    search_fields = ('title', )
+    prepopulated_fields = {'slug': ('title', )}
