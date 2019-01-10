c027c338379a93ec561baa8db74ac0aae53ee19e
Solomon Nsubuga <snsubuga@live.com>
2019-01-10 11:57:04 +0300

    chore(articles-list): View list of articles (#5)
    - add a articles list view
    - paginate and categorize articles
    - add article list html

--- articles/urls.py
+++ articles/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import ArticleListView
 
 app_name = 'articles'
 
 urlpatterns = [
-
+    path('', ArticleListView.as_view(), name='article_list')
 ]
 
 # Bad idea don't do this in production
