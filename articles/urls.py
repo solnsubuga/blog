26c0def9b69df3b10a26cd70dff05597c0644f25
Nsubuga Solomon <snsubuga@live.com>
2019-01-10 16:24:54 +0300

    [Feature] Add image upload to AWS S3

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import ArticleListView, ArticleDetailView
@@ -10,7 +9,3 @@ urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>/<slug>/', ArticleDetailView.as_view(), name='article_detail')
 ]
-
-# Bad idea don't do this in production
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
